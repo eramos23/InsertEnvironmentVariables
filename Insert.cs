808b8d48449dfca2a4eb01041a302797a6c7197b
Ely Ramos Yañe <raicer1996@gmail.com>
2023-12-19 19:55:39 -0500

    Add files via upload
    dev

---
+++ Insert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertEnvironmentVariables
+{
+    internal class Insert
+    {
+        public void InsertVariable()
+        {
+            string txtFile = $"{Environment.CurrentDirectory}/txtFile.txt";
+            Console.WriteLine(txtFile);
+            Console.WriteLine();
+            string[] lines = File.ReadAllLines("");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace InsertEnvironmentVariables
+{
+    class Program
+    {
+        static readonly string textFile = $"{Environment.CurrentDirectory}\\txtFile.txt";
+
+        static void Main(string[] args)
+        {
+            if (File.Exists(textFile))
+            {
+                string[] lines = File.ReadAllLines(textFile);
+                foreach (string line in lines)
+                {
+                    string variable = line.Split('=')[0];
+                    string value = line.Split("=")[1];
+
+                    if (Environment.GetEnvironmentVariable(variable) == null)
+                    {
+                        Environment.SetEnvironmentVariable(variable, value, EnvironmentVariableTarget.Machine);
+                        Console.WriteLine($"Inseted: {line}");
+                    } else
+                    {
+                        Console.WriteLine($"Already existe: {line}");
+                    }
+                }
+                
+            } else {
+                Console.WriteLine("dont exist file");
+            }
+            Console.ReadKey();
+        }
+    }
+}
